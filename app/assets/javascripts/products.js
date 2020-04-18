412e963e4a7b8c4e0d6831df4d2edc7e79d3711c
Matias Renta <matiasrenta@gmail.com>
2020-04-18 13:28:05 -0300

    mejora en formulario de productos

--- app/assets/javascripts/products.js
+++ app/assets/javascripts/products.js
@@ -3,31 +3,35 @@
 $('#product_product_cost').on('input', function() {
     calculate_total_cost_and_price();
 });
+
 $('#product_cargo_cost').on('input', function() {
     calculate_total_cost_and_price();
 });
+
 $('#product_profit_percent').on('input', function() {
-    calculate_total_cost_and_price();
+    calculate_price();
 });
 
 $('#product_sale_price').on('input', function() {
-    if($.isNumeric($('#product_total_cost').val()) && $.isNumeric($('#product_sale_price').val()) ){
-        var sale_price = parseFloat($('#product_sale_price').val());
-        var total_cost = parseFloat($('#product_total_cost').val());
-        $('#product_profit_percent').val( ((sale_price - total_cost) / total_cost) * 100 );
-    }
+    calculate_profit_percent();
 });
 
 
 function calculate_total_cost_and_price(){
+    calculate_total_cost();
+    calculate_price();
+}
+
+function calculate_total_cost(){
     if( $.isNumeric($('#product_product_cost').val()) && $.isNumeric($('#product_cargo_cost').val()) ){
         $('#product_total_cost').val(parseFloat($('#product_product_cost').val()) + parseFloat($('#product_cargo_cost').val()));
     }
     else {
         $('#product_total_cost').val('');
     }
+}
 
-
+function calculate_price(){
     if($.isNumeric($('#product_total_cost').val()) && $.isNumeric($('#product_profit_percent').val()) ){
         var total_cost = parseFloat($('#product_total_cost').val());
         var profit_percent = parseFloat($('#product_profit_percent').val());
@@ -38,4 +42,10 @@ function calculate_total_cost_and_price(){
     }
 }
 
-
+function calculate_profit_percent(){
+    if($.isNumeric($('#product_total_cost').val()) && $.isNumeric($('#product_sale_price').val()) ){
+        var sale_price = parseFloat($('#product_sale_price').val());
+        var total_cost = parseFloat($('#product_total_cost').val());
+        $('#product_profit_percent').val( ((sale_price - total_cost) / total_cost) * 100 );
+    }
+}

a2cc4cd51ec6b87b43e3aff83c2d31f0ca157b73
Matias Renta <matiasrenta@gmail.com>
2020-04-25 08:31:59 -0300

    better product select in orders

--- app/assets/javascripts/orders.js
+++ app/assets/javascripts/orders.js
@@ -1,10 +1,11 @@
-// cuando agrego item en el formulario le quito los campos disabled
+// cuando agrego item en el formulario le quito los campos disabled y le agrego select2() al dropdown
 $(document).on('nested:fieldAdded', function(event){
     // this field was just inserted into your form
     var field = event.field;
     // it's a jQuery object already! Now you can find date input
     var fields_disabled = field.find('.enable_me');
     fields_disabled.prop("disabled", false);
+    field.find("select").select2();
 });
 
 // cuando elimino un item del formulario calculo el total_amount

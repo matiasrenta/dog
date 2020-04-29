141f9f61e61808af3f7a9eae0f0fddf5d8c7a6f6
Matias Renta <matiasrenta@gmail.com>
2020-04-29 14:52:47 -0300

    set false to all booleans in NULL

--- app/models/dev/catalog_cleaner.rb
+++ app/models/dev/catalog_cleaner.rb
@@ -1,6 +1,22 @@
 class CatalogCleaner < Object
-  ##@@all_catalogs = ['cat_der_human_right','cat_der_strategy','cat_der_line_of_action','cat_gen_axi','cat_gen_objective','cat_gen_strategy','cat_gen_goal','cat_gen_line_of_action','cat_pgd_axi','cat_pgd_area_of_opportunity','cat_pgd_objective','cat_pgd_goal','cat_pgd_line_of_action','cat_fon_funding_source','cat_fon_generic_source','cat_fon_specific_source','cat_fon_year_document','cat_fon_origin_resource','cat_fon_fund','cat_cfu_finality','cat_cfu_function','cat_cfu_subfunction','cat_ere_expending_focu','cat_ere_result','cat_ere_subresult','cat_ppr_expense_type','cat_ppr_digit_identifier','cat_ppr_spending_destination','cat_ppr_par_chapter','cat_ppr_par_concept','cat_ppr_par_partida_generica','cat_ppr_par_partida_especifica','cat_are_area','cat_uni_measure_unit']
   @@all_catalogs = ['cat_der_human_right','cat_der_strategy','cat_der_line_of_action','cat_gen_axi','cat_gen_objective','cat_gen_strategy','cat_gen_goal','cat_gen_line_of_action','cat_pgd_axi','cat_pgd_area_of_opportunity','cat_pgd_objective','cat_pgd_goal','cat_pgd_line_of_action','cat_fon_funding_source','cat_fon_generic_source','cat_fon_specific_source','cat_fon_year_document','cat_fon_origin_resource','cat_fon_fund','cat_cfu_finality','cat_cfu_function','cat_cfu_subfunction','cat_ere_expending_focu','cat_ere_result','cat_ere_subresult','cat_ppr_expense_type','cat_ppr_digit_identifier','cat_ppr_par_chapter','cat_ppr_par_concept','cat_ppr_par_partida_generica','cat_ppr_par_partida_especifica','cat_are_area','cat_uni_measure_unit']
+
+
+
+  def self.set_false_to_nil_booleans
+    sql = 'UPDATE products SET is_mix_box = FALSE WHERE is_mix_box IS NULL'
+    ActiveRecord::Base.connection.execute(sql)
+
+    sql = 'UPDATE products SET units_sale_allowed = FALSE WHERE units_sale_allowed IS NULL'
+    ActiveRecord::Base.connection.execute(sql)
+
+    sql = 'UPDATE orders SET iva = FALSE WHERE iva IS NULL'
+    ActiveRecord::Base.connection.execute(sql)
+  end
+
+
+
+  # PARA ABJAO SON EJEMPLOS DE LO QUE SE PUEDE HACER
 
   def self.clean_catalogs
     @@all_catalogs.each do |catalog|

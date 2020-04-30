9691aca967e46c942a004ae55cc90cd62613e40e
Matias Renta <matiasrenta@gmail.com>
2020-04-29 23:18:20 -0300

    price and profit percent changeable

--- app/assets/javascripts/products.js
+++ app/assets/javascripts/products.js
@@ -17,4 +17,21 @@ function calculate_total_cost(){
     }
 }
 
+// cuando ingreso el margen de ganancia (profit_percent) calculo el precio
+$(document).on("input", "input[id$='_profit_percent']", function() {
+    profit_percent = parseFloat(this.value);
+    product_total_cost = parseFloat($("#product_total_cost").val());
+    price = product_total_cost + (product_total_cost * (profit_percent / 100));
+    product_price_element = $( this.closest('tr') ).find("input[id$='_price']")[0];
+    product_price_element.value = price;
+});
+
+// cuando ingreso el precio calculo el margen de ganancia (profit_percent)
+$(document).on("input", "input[id$='_price']", function() {
+    price = parseFloat(this.value);
+    product_total_cost = parseFloat($("#product_total_cost").val());
+    profit_percent = ((price - product_total_cost) / product_total_cost) * 100
+    product_price_element = $( this.closest('tr') ).find("input[id$='_profit_percent']")[0];
+    product_price_element.value = profit_percent;
+});
 

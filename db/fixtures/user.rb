5b0147afd29010ad966306be5b249492c1fdd4f5
Matias Renta <matiasrenta@gmail.com>
2020-05-27 09:55:33 -0300

    seed fu fixed

--- db/fixtures/user.rb
+++ db/fixtures/user.rb
@@ -6,23 +6,28 @@ User.seed_once(:email) do |s|
   # esta linea da error la primera vez que se deploya. correr bundle exec rake db:seed_fu otra vez
   # (o deployar si es en prod) y entonces encontrará el rol 'superuser' cargado en la bbdd
   s.role_id = Role.find_by_name('superuser').id
+end
 
+User.seed_once(:email) do |s|
   s.email = 'alejandroperrotat@gmail.com'
   s.name = 'Perro'
   s.password = 'recompensa'
   s.password_confirmation = 'recompensa'
   s.role_id = Role.find_by_name('Administrador').id
+end
 
+User.seed_once(:email) do |s|
   s.email = 'administrador@mail.com'
   s.name = 'Administrador'
   s.password = 'recompensa'
   s.password_confirmation = 'recompensa'
   s.role_id = Role.find_by_name('Administrador').id
+end
 
+User.seed_once(:email) do |s|
   s.email = 'vendedor@mail.com'
   s.name = 'Vendedor'
   s.password = 'recompensa'
   s.password_confirmation = 'recompensa'
   s.role_id = Role.find_by_name('Vendedor').id
-
 end

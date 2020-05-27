a0a7f017aba12cfd3093c14e6b65445f31e0c838
Matias Renta <matiasrenta@gmail.com>
2020-05-27 08:31:51 -0300

    fixtures for stagin

--- db/fixtures/role.rb
+++ db/fixtures/role.rb
@@ -1,14 +1,14 @@
 Role.seed_once(:name) do |r|
-  r.name = "superuser"
+  r.name = 'superuser'
   r.list_order = 1
 end
 
 Role.seed_once(:name) do |r|
-  r.name = "Administrador"
+  r.name = 'Administrador'
   r.list_order = 2
 end
 
 Role.seed_once(:name) do |r|
-  r.name = "Vendedor"
+  r.name = 'Vendedor'
   r.list_order = 3
 end
--- db/fixtures/user.rb
+++ db/fixtures/user.rb
@@ -1,10 +1,28 @@
 User.seed_once(:email) do |s|
-  s.email = "superuser@mail.com"
-  s.name = "SuperUser"
+  s.email = 'superuser@mail.com'
+  s.name = 'Super User'
   s.password = 'recompensa'
   s.password_confirmation = 'recompensa'
-
   # esta linea da error la primera vez que se deploya. correr bundle exec rake db:seed_fu otra vez
   # (o deployar si es en prod) y entonces encontrará el rol 'superuser' cargado en la bbdd
   s.role_id = Role.find_by_name('superuser').id
+
+  s.email = 'alejandroperrotat@gmail.com'
+  s.name = 'Perro'
+  s.password = 'recompensa'
+  s.password_confirmation = 'recompensa'
+  s.role_id = Role.find_by_name('Administrador').id
+
+  s.email = 'administrador@mail.com'
+  s.name = 'Administrador'
+  s.password = 'recompensa'
+  s.password_confirmation = 'recompensa'
+  s.role_id = Role.find_by_name('Administrador').id
+
+  s.email = 'vendedor@mail.com'
+  s.name = 'Vendedor'
+  s.password = 'recompensa'
+  s.password_confirmation = 'recompensa'
+  s.role_id = Role.find_by_name('Vendedor').id
+
 end
